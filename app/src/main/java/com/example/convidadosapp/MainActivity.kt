dc6e83e10440a7a547657c9bd67cb2d56dd4a212
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-29 09:42:44 -0300

    Ajustando nomenclaturas

--- app/src/main/java/com/example/convidadosapp/MainActivity.kt
+++ app/src/main/java/com/example/convidadosapp/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_all_guests, R.id.nav_present, R.id.nav_absent
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/convidadosapp/ui/AllGuestsModel.kt
+++ app/src/main/java/com/example/convidadosapp/ui/AllGuestsModel.kt
@@ -1,13 +1,13 @@
-package com.example.convidadosapp.ui.slideshow
+package com.example.convidadosapp.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AllGuestsModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }

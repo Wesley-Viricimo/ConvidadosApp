5a4020821ed45dd82d6058a11301703d0f8f6f79
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-11-02 15:50:38 -0300

    Realizando a criação de lista para receber os convidados

--- app/src/main/java/com/example/convidadosapp/viewmodel/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/convidadosapp/viewmodel/AllGuestsViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.convidadosapp.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.convidadosapp.model.GuestModel
+import com.example.convidadosapp.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val repository: GuestRepository = GuestRepository.getInstance(application.applicationContext)
+
+    private val listAllGuests = MutableLiveData<List<GuestModel>>() //Preparando a variável para receber um lista mutável de convidados
+    val guests: LiveData<List<GuestModel>> = listAllGuests //Recebendo a lista utilizando LiveData para ser utilizado o observável
+
+    fun getAll() {
+        listAllGuests.value = repository.getAll() //Atribuindo convidados a lista de convidados
     }
-    val text: LiveData<String> = _text
 }

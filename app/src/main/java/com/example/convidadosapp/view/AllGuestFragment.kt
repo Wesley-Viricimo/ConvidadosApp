4c2200435c0f077802de0596aca2abdcb45d495b
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-11-04 17:24:17 -0300

    Criando adapter e view holder para listar convidados

--- app/src/main/java/com/example/convidadosapp/view/AllGuestFragment.kt
+++ app/src/main/java/com/example/convidadosapp/view/AllGuestFragment.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidadosapp.databinding.FragmentAllGuestsBinding
+import com.example.convidadosapp.view.adapter.GuestsAdapter
 import com.example.convidadosapp.viewmodel.AllGuestsViewModel
 
 class AllGuestFragment : Fragment() {
@@ -16,21 +19,19 @@ class AllGuestFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var viewModel: AllGuestsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-     viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+
+        _binding!!.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
+
+        binding.recyclerAllGuests.adapter = GuestsAdapter()
 
         viewModel.getAll()
 
         observe()
 
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/convidadosapp/view/adapter/GuestsAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.convidadosapp.view.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.convidadosapp.view.viewholder.GuestViewHolder
+
+class GuestsAdapter : RecyclerView.Adapter<GuestViewHolder>() { //Adapter é responsável por realizar a junção do layout com os dados
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GuestViewHolder { //Método responsável pela criação do layout, será chamado para cada item da lista
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GuestViewHolder, position: Int) { //Responsável por atribuir os dados para o layout
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int { //Método responsável pelo tamanho da listagem
+        TODO("Not yet implemented")
+    }
+}
